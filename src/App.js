17ca3687370d6cf3180a2bc5248f3b58fcc4a8d1
marcorag <marcoragusa77@gmail.com>
2022-07-06 16:02:20 +0200

    divertitevi

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import {  useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
 import Card from './Card';
 
 function App() {
-  const ANNO_CORRENTE = '2022';
+
   const mioNome = 'Marco';
   const mioCognome = 'Ragusa';
   const[testStato, setTestStato]=useState(false);
   return (
-    <div className="App">
-      <Counter></Counter>
-      <header className="App-header" >
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to . 
-          {(testStato) && <Card nomePersona = {mioNome} cognomePersona = {mioCognome} />}
-          
-        </p>
-        <button onClick={()=>setTestStato(!testStato)}>cambia colore</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className='header-container'>
+        <div className='logo'>Logo</div>
+        <nav className='navbar-container'>
+          <a href=''>Link 1</a>
+          <a href=''>link 2</a>
+          <a href=''>link 3</a>
+        </nav>
       </header>
+      <div className='hero-section'></div>
+      <footer></footer>
     </div>
   );
 }
