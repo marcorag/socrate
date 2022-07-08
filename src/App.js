8481d4be521d4b5b5bb3b927627f2fc8b468c997
MicheleF4 <michelefano.learn@gmail.com>
2022-07-08 15:40:01 +0200

    update header1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+/*DATA*/
+import listLinkData from "./components/listLinkData";
+/*COMPONENTS*/
+import Header from "./components/Header.component";
+import Hero from "./components/Hero.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header data={listLinkData} />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/listLinkData.js
@@ -0,0 +1,7 @@
+export default [
+  { id: 1, nome: "Home", url: "#" },
+  { id: 2, nome: "Corsi", url: "#" },
+  { id: 3, nome: "Docenti", url: "#" },
+  { id: 4, nome: "Contatti", url: "#" },
+  { id: 5, nome: "Chi siamo", url: "#" },
+];
